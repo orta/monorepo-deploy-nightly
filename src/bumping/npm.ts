6d5f4e04af151f1ba0534b3ea10c5aad9fe9bd21
Simon Holthausen <simon.holthausen@accso.de>
2020-07-28 17:19:42 +0200

    Bump npm version to package.json's version if it is higher

--- src/bumping/npm.ts
+++ src/bumping/npm.ts
@@ -7,12 +7,12 @@ const axios = require("axios").default;
 
 const getPackageVersion = async (packageMD: PackageMetadata) => {
   try {
-    const npmInfo = await axios({ url:`https://registry.npmjs.org/${packageMD.name}`, method: "GET" });
+    const npmInfo = await axios({ url: `https://registry.npmjs.org/${packageMD.name}`, method: "GET" });
     if (!npmInfo.data || !npmInfo.data._id) {
       throw new Error("Got a bad response from NPM");
-    } 
+    }
     return npmInfo.data['dist-tags'].latest;
-    
+
   } catch (error) {
     console.log(`${packageMD.name} is a new package, starting from version in package.json`)
     const pkgPath = join(packageMD.path, "package.json");
@@ -21,17 +21,36 @@ const getPackageVersion = async (packageMD: PackageMetadata) => {
   }
 };
 
+const isPackageJSONVersionHigher = (packageJSONVersion: string, bumpedVersion: string) => {
+  const semverMarkersPackageJSON = packageJSONVersion.split(".");
+  const semverMarkersBumped = bumpedVersion.split(".");
+  for (let i = 0; i < 3; i++) {
+    if (Number(semverMarkersPackageJSON[i]) > Number(semverMarkersBumped[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+const bumpPatch = (version: string) => {
+  const semverMarkers = version.split(".");
+  return `${semverMarkers[0]}.${semverMarkers[1]}.${Number(semverMarkers[2]) + 1}`;
+}
+
 export const bumpVersionNPM = async (packageMD: PackageMetadata) => {
   const version = await getPackageVersion(packageMD);
   if (!version) throw new Error("Could not find the npm version in the registry");
 
-  const semverMarkers = version.split(".");
-  const newVersion = `${semverMarkers[0]}.${semverMarkers[1]}.${Number(semverMarkers[2]) + 1}`;
-
   const pkgPath = join(packageMD.path, "package.json");
-  const oldPackageJSON = JSON.parse(readFileSync(pkgPath, "utf8"));
-  oldPackageJSON.version = newVersion;
-  writeFileSync(pkgPath, JSON.stringify(oldPackageJSON));
+  const packageJSON = JSON.parse(readFileSync(pkgPath, "utf8"));
+
+  const newVersion = isPackageJSONVersionHigher(packageJSON.version, version)
+    ? packageJSON.version
+    : bumpPatch(version);
+
+  packageJSON.version = newVersion;
+  writeFileSync(pkgPath, JSON.stringify(packageJSON));
 
   console.log(`Updated ${packageMD.name} to ${newVersion} from npm`);
 };
